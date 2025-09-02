4696df485d6367c28eab08352d5299e1701c9f3d
JaeHyeok <yang55923@gmail.com>
2025-09-02 16:42:38 +0900

    init

---
+++ src/test/java/org/example/seasonthon/SeasonThonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.seasonthon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeasonThonApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
