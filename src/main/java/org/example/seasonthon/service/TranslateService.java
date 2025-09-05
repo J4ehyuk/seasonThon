15a34407e4e90a65c0085603db00e7877c8177cc
JaeHyeok <yang55923@gmail.com>
2025-09-05 16:18:28 +0900

    deploy2
    번역 컨트롤러 주석처리

--- src/main/java/org/example/seasonthon/service/TranslateService.java
+++ src/main/java/org/example/seasonthon/service/TranslateService.java
@@ -10,30 +10,30 @@ import org.springframework.web.client.RestClient;
 import java.util.HashMap;
 import java.util.Map;
 
-@Service
-public class TranslateService {
-
-  private final RestClient http;
-
-  public TranslateService(@Value("${translate.base-url}") String baseUrl) {
-    this.http = RestClient.builder()
-            .baseUrl(baseUrl)
-            .build();
-  }
-
-  public TranslateResponse translate(TranslateRequest req) {
-    Map<String, Object> body = new HashMap<>();
-    body.put("q", req.q());
-    body.put("source", req.source() == null ? "auto" : req.source());
-    body.put("target", req.target() == null ? "ko" : req.target());
-    body.put("format", req.format() == null ? "text" : req.format());
-
-    return http.post()
-            .uri("/translate")
-            .contentType(MediaType.APPLICATION_JSON)
-            .body(body)
-            .retrieve()
-            .toEntity(TranslateResponse.class)
-            .getBody();
-  }
-}
+//@Service
+//public class TranslateService {
+//
+//  private final RestClient http;
+//
+//  public TranslateService(@Value("${translate.base-url}") String baseUrl) {
+//    this.http = RestClient.builder()
+//            .baseUrl(baseUrl)
+//            .build();
+//  }
+//
+//  public TranslateResponse translate(TranslateRequest req) {
+//    Map<String, Object> body = new HashMap<>();
+//    body.put("q", req.q());
+//    body.put("source", req.source() == null ? "auto" : req.source());
+//    body.put("target", req.target() == null ? "ko" : req.target());
+//    body.put("format", req.format() == null ? "text" : req.format());
+//
+//    return http.post()
+//            .uri("/translate")
+//            .contentType(MediaType.APPLICATION_JSON)
+//            .body(body)
+//            .retrieve()
+//            .toEntity(TranslateResponse.class)
+//            .getBody();
+//  }
+//}
