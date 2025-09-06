2f166fa7b5b6508260cc3ac75469c3e5036d740f
JaeHyeok <yang55923@gmail.com>
2025-09-06 18:09:15 +0900

    swegger 수정
    검색위치를 강남역으로 기본으로 고정

--- src/main/java/org/example/seasonthon/dto/HospitalLocRequestDto.java
+++ src/main/java/org/example/seasonthon/dto/HospitalLocRequestDto.java
@@ -7,10 +7,10 @@ import lombok.Setter;
 // 현재 사용자의 위치 전달
 public record HospitalLocRequestDto(
 
-        @Schema(description = "현재 사용자의 위도", example = "35.876")
+        @Schema(description = "현재 사용자의 위도", example = "37.49877474832768")
         Double latitude,  // 위도
 
-        @Schema(description = "현재 사용자의 경도", example = "128.597")
+        @Schema(description = "현재 사용자의 경도", example = "127.0278195920194")
         Double longitude, // 경도
 
         @Schema(description = "선호하는 언어 (예: English, Japanese, Chinese)", example = "English")
